a9fc97c72f21d8b5749c8a15c2ba5271baab3e1a
Coderbro <mo.abbaszadi@gmail.com>
2022-02-22 17:31:35 +0000

    groupie-tracker

--- main.go
+++ main.go
@@ -16,25 +16,81 @@ type GroupieTracker struct {
 	RelationUrl  string `json:"relation"`
 }
 
-func main() {
-	var groupieTrackerObject GroupieTracker
+type Artists struct {
+	Id           int	   `json:"id"`
+	Image        string	   `json:"image"`
+	Name         string	   `json:"name"`
+	Members      []string  `json:"members"`
+	CreationDate int	   `json:"creationDate"`
+	FirstAlbum   string	   `json:"firstAlbum"`
+	Locations    string	   `json:"locations"`
+	ConcertDates string	   `json:"concertDates"`
+	Relations    string	   `json:"relations"`
+}
 
-	response, err := http.Get("https://groupietrackers.herokuapp.com/api")
+// type Locations struct {
+// 	Id        int
+// 	Locations []string
+// 	Dates     string
+// }
+
+// type Dates struct {
+// 	Id    int
+// 	Dates []string
+// }
+
+// type Relations struct {
+// 	Id             int
+// 	DatesLocations map[int]string
+// }
+
+func main() {
+	// var groupieTrackerObject GroupieTracker
+
+	// response, err := http.Get("https://groupietrackers.herokuapp.com/api")
+	// if err != nil {
+	// 	fmt.Print(err.Error())
+	// 	os.Exit(1)
+	// }
+
+	// responseData, err := ioutil.ReadAll(response.Body)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Println(string(responseData))
+	// json.Unmarshal(responseData, &groupieTrackerObject)
+	// fmt.Println(groupieTrackerObject.ArtistsUrl)
+	// fmt.Println(groupieTrackerObject.LocationsUrl)
+	// fmt.Println(groupieTrackerObject.DatesUrl)
+	// fmt.Println(groupieTrackerObject.RelationUrl)
+
+
+	var artistsObject []Artists // []Artists as we have many artists; links to the Artists struct and stores as a variable
+
+	response2, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
 	if err != nil {
 		fmt.Print(err.Error())
 		os.Exit(1)
 	}
 
-	responseData, err := ioutil.ReadAll(response.Body)
+	responseData2, err := ioutil.ReadAll(response2.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(string(responseData))
-	json.Unmarshal(responseData, &groupieTrackerObject)
-	fmt.Println(groupieTrackerObject.ArtistsUrl)
-	fmt.Println(groupieTrackerObject.LocationsUrl)
-	fmt.Println(groupieTrackerObject.DatesUrl)
-	fmt.Println(groupieTrackerObject.RelationUrl)
+	// fmt.Println(string(responseData2))
+	json.Unmarshal(responseData2, &artistsObject)
+	fmt.Println(artistsObject[0].Id)
+	fmt.Println(artistsObject[0].Image)
+	fmt.Println(artistsObject[0].Name)
+	fmt.Println(artistsObject[0].Members)
+	fmt.Println(artistsObject[0].CreationDate)
+	fmt.Println(artistsObject[0].FirstAlbum)
+	fmt.Println(artistsObject[0].Locations)
+	fmt.Println(artistsObject[0].ConcertDates)
+	fmt.Println(artistsObject[0].Relations)
+
+
+
 
 }
 
